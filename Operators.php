c20f27023eb8745a897749ac40bd8a85a2a1a83e
Mayank Singh <mayankdurani@gmail.com>
2025-07-29 23:02:19 +0530

    Arthematic_Operators

---
+++ Operators.php
@@ -0,0 +1,11 @@
+<?php
+    $a = 45;
+    $b = 50;
+    // Arthematic Operators
+    echo "For a + b, the result is ".($a + $b)."<br>";
+    echo "For a - b, the result is ".($a - $b)."<br>";
+    echo "For a / b, the result is ".($a / $b)."<br>";
+    echo "For a * b, the result is ".($a * $b)."<br>";
+    echo "For a % b, the result is ".($a % $b)."<br>";
+    echo "For a ** b, the result is ".($a ** $b)."<br>";
+?>
