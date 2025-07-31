ddcce991764b0d2f86eb71ac227246a0b256ad59
Mayank Singh <mayankdurani@gmail.com>
2025-07-31 19:56:19 +0530

    Age output using switch

---
+++ Switch_case.php
@@ -0,0 +1,17 @@
+<?php
+
+$age = 4;
+
+switch($age){
+
+    case 2:
+        echo "Your are 12 years old <br>";
+        break;
+    case 4:
+        echo "Your are 14 years old <br>";
+        break;
+    default:
+        echo "Enter LIMITED years old <br>";
+        break;
+     }
+?>
